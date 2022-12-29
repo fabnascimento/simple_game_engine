cdf60ab41edd0622f08f62cc308a84a82786cb1e
Fabricio Nascimento <f.f.nascimento9@gmail.com>
2022-12-29 13:05:20 -0500

    feat: add capped game loop

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,8 @@
 #include "Game.h"
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <glm/glm.hpp>
 
 Game::Game(/* args */)
 {
@@ -13,52 +15,113 @@ Game::~Game()
     std::cout << "Game destructor was called" << std::endl;
 }
 
-void Game::Initialize() {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+void Game::Initialize()
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
         std::cerr << "Error initializing SDL." << std::endl;
         return;
     }
+
+    SDL_DisplayMode displayMode;
+    SDL_GetCurrentDisplayMode(0, &displayMode);
+    // this could lead to some scaling issues FAKE/REAL Fullscreen
+    windowWidth = displayMode.w;
+    windowHeight = displayMode.h;
 
     window = SDL_CreateWindow(
         NULL,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
-        SDL_WINDOW_BORDERLESS
-    );
+        windowWidth,
+        windowHeight,
+        SDL_WINDOW_BORDERLESS);
 
-    if (!window) {
+    if (!window)
+    {
         std::cerr << "Error creating SDL WINDOW" << std::endl;
         return;
     }
 
-    renderer = SDL_CreateRenderer(window, -1, 0); // renderer to draws to
+    // renderer that SDL uses to draw to
+    // TODO: check SDL Flags for VSYNC and GPU Acceleration
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-    if (!renderer) {
+    if (!renderer)
+    {
         std::cerr << "Error creating SDL Renderer" << std::endl;
         return;
     }
 
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+
     isRunning = true;
 }
 
-void Game::ProcessInput() {
+void Game::ProcessInput()
+{
     SDL_Event sdlEvent;
-    while (SDL_PollEvent(&sdlEvent)) {
-        switch (sdlEvent.type) {
-            case SDL_QUIT:
+    while (SDL_PollEvent(&sdlEvent))
+    {
+        switch (sdlEvent.type)
+        {
+        case SDL_QUIT:
+            isRunning = false;
+            break;
+        case SDL_KEYDOWN:
+            if (sdlEvent.key.keysym.sym == SDLK_ESCAPE)
+            {
                 isRunning = false;
-                break;
-            case SDL_KEYDOWN:
-                if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
-                    isRunning = false;
-                }
+            }
         }
     }
 }
 
-void Game::Run() {
+glm::vec2 playerPosition;
+glm::vec2 playerVelocity;
+
+void Game::Setup()
+{
+    playerPosition = glm::vec2(10.0, 20.0);
+    playerVelocity = glm::vec2(1.0, 0.0);
+}
+
+void Game::Update()
+{
+    int timeToWait = MS_PER_FRAME - (SDL_GetTicks() - msPreviousFrame);
+    if (timeToWait > 0 && timeToWait <= MS_PER_FRAME){
+        SDL_Delay(timeToWait);
+    }
+    msPreviousFrame = SDL_GetTicks();
+
+    playerPosition.x += playerVelocity.x;
+    playerPosition.y += playerVelocity.y;
+}
+
+void Game::Render()
+{
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // TODO: Render game objects
+    SDL_Surface *surface = IMG_Load("./assets/images/tank-tiger-right.png");
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    SDL_Rect destinationRect = {
+        static_cast<int>(playerPosition.x),
+        static_cast<int>(playerPosition.y),
+        32,
+        32};
+    SDL_RenderCopy(renderer, texture, NULL, &destinationRect);
+    SDL_DestroyTexture(texture);
+
+    SDL_RenderPresent(renderer);
+}
+
+void Game::Run()
+{
+    Setup();
     while (isRunning)
     {
         ProcessInput();
@@ -67,20 +130,8 @@ void Game::Run() {
     }
 }
 
-void Game::Update() {
-    // TODO: update game objects
-}
-
-void Game::Render() {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    // TODO: Render game objects
-
-    SDL_RenderPresent(renderer);
-}
-
-void Game::Destroy() {
+void Game::Destroy()
+{
     // TODO: destroy allocated resources
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
--- src/Game.h
+++ src/Game.h
@@ -3,9 +3,13 @@
 
 #include <SDL2/SDL.h>
 
+const Uint8 FPS = 60;
+const Uint8 MS_PER_FRAME = 1000 / FPS;
+
 class Game {
     private:
         bool isRunning;
+        Uint64 msPreviousFrame = 0;
         SDL_Window* window;
         SDL_Renderer* renderer;
     public:
@@ -13,10 +17,14 @@ class Game {
         ~Game();
         void Initialize();
         void Run();
+        void Setup();
         void ProcessInput();
         void Update();
         void Render();
         void Destroy();
+
+        int windowWidth;
+        int windowHeight;
 };
 
 #endif
